75d6144d70cd9d691c58b79cc24a0b4d368c4af0
Marcus Almeida <marcusmage@gmail.com>
2016-08-24 12:40:09 -0300

    changing the layout a little bit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 ---
 layout: null
 ---
-$(document).ready(function () {
-  $('a.blog-button').click(function (e) {
+var AnimateOnLoad = function(){
+    //$('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
@@ -12,7 +12,11 @@ $(document).ready(function () {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
-  })
+  //}) 
+};
+
+$(document).ready(function () {
+  setTimeout(AnimateOnLoad, 250);
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
